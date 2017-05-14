a8e2c413b6df0a46a95efc90d0e4a3fc2848e5c5
Lukas Edström <lukasedstrom@gmail.com>
2017-05-14 22:21:05 +0200

    update

--- Projekt - Ny/jquery.js
+++ Projekt - Ny/jquery.js
@@ -1,3 +1,19 @@
-if($(window).scrollTop() + $(window).height() > 150) {
-	$("#menu").addClass("menuOpacity")
-}; 
+
+
+$(window).on('scroll', function() {
+     if($(window).scrollTop() > 150) {
+         $("#menu").addClass("menuOpacity");
+}    else {
+        $("#menu").removeClass("menuOpacity");
+        }
+})
+
+
+$(document).ready(function(){
+  $("a").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
